9593214c969eca337f99e42c48624f672c2a5c3c
Kaseke <kasymkhankhubievnis@gmail.com>
2023-05-02 21:12:01 +0700

    implemented function "compute_distances_two_loops"

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -55,7 +55,7 @@ class KNN:
         for i_test in range(num_test):
             for i_train in range(num_train):
                 # TODO: Fill dists[i_test][i_train]
-                pass
+                dists[i_test, i_train] = np.sum(np.abs(X[i_test] - self.train_X[i_train]))
 
     def compute_distances_one_loop(self, X):
         '''
